4f2b38b39a3c99bb84b8002408271550c8420561
Jennings Anderson <jenningsanderson@colorado.edu>
2019-11-20 11:51:59 -0700

    Automagically building YurtSki from moba

--- js/site.js
+++ js/site.js
@@ -34,3 +34,24 @@ window.addEventListener('load', function(){
     }
   })
 })
+
+const CHECKFRONT_API = 'https://yurtski.checkfront.com/api/3.0/';
+
+function hitCheckfrontAPI(endpoint, callback){
+  var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            // console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", CHECKFRONT_API+endpoint, true);
+    xmlhttp.send();
+}
